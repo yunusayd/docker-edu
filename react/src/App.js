fa5282533aa4ff16584adea5bdd21705fa7334a0
yunusayd <yunusayd@gmail.com>
2020-05-18 19:58:12 +0300

    docker-compose react

--- react/src/App.js
+++ react/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          UnLearn Vue please!!!
+          UnLearn Vue please!!
         </a>
       </header>
     </div>
